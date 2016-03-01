c9b62acdb993f3a460663a5a914fb2f3b32586b0
Kamil Chlebek <kamil.chlebek@imac-11.local>
2016-03-01 18:29:32 +0100

    nothing important

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
+    private static final String input = "72 * x12 + 175";
+
     public static void main(String[] args) {
+
+        String[] split = input.split("");
+
+        ArrayList<String> strings = new ArrayList<String>(Arrays.asList(split));
+        ArrayList<Token> output = new ArrayList<>
+        for (int i = 0; i < strings.size(); i++) {
+
+        }
+
         System.out.println("Hello World!");
     }
 }
